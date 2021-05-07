8cd1e603985c2daf6579708901655fc9fd9b5155
Patrick <tunezepatrick@gmail.com>
2021-05-07 11:14:13 +0200

    login

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./_homeScreen.scss";
+import catsImage from "../../files/images/two-cats.svg";
+
+const HomeScreen = () => {
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img src={catsImage} alt="Cats!" />
+        <h1>Welcome to Tasker</h1>
+        <button type="submit">Login with Gmail</button>
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
